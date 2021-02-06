121a42daff77ec2fd9270c66c41f1509cb058f92
MMHO <MMHO@itu.dk>
2021-02-06 13:32:46 +0100

    added some test images and moved some of the DepthEstimation code from test.py to DepthEstimator.py. Made a IOHelper script for handling loading and showing images and videos

---
+++ DepthEstimation/IOHelper.py
@@ -0,0 +1,15 @@
+
+def showImage(name, image):
+    raise NotImplementedError
+
+def loadImage(path):
+    raise NotImplementedError
+
+def captureScreenLive():
+    raise NotImplementedError
+
+def loadVideo(path):
+    raise NotImplementedError
+
+def showVideo(video):
+    raise NotImplementedError
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.dirname('CollisionDetection/'))) 
+sys.path.insert(0, os.path.abspath(os.path.dirname('CollisionDetection/')))
 sys.path.insert(0, os.path.abspath(os.path.dirname('Controller/'))) 
 sys.path.insert(0, os.path.abspath(os.path.dirname('DepthEstimation/'))) 
 sys.path.insert(0, os.path.abspath(os.path.dirname('RoutePlanner/'))) 
